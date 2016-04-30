d81138a3fc54c9cbe395c559593e1096895b57a8
Limmen <kimham@kth.se>
2016-04-30 23:16:01 +0200

    add react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+    </Router>
+    , document.getElementById('app'));
