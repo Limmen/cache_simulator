70f75e9c8db084c2d7b5d81dd87d89f4b61a8854
Limmen <kimham@kth.se>
2016-05-10 11:57:42 +0200

    update favicon and metadata

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('core-js/fn/object/assign');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+const config = require('./webpack.config.js');
 const open = require('open');
 
 new WebpackDevServer(webpack(config), config.devServer)
