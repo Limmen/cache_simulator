c1a300440c2e5773f29739f281840597396c2fdc
Limmen <kimham@kth.se>
2016-06-19 19:10:54 +0200

    add logging to file for production

--- src/server/util/logger.js
+++ src/server/util/logger.js
@@ -7,31 +7,52 @@
 import winston from 'winston'
 
 winston.emitErrs = true;
+const env = process.env.NODE_ENV || 'dev';
+if(env === "production") {
+  let logger = new winston.Logger({
+    transports: [
+      new winston.transports.File({
+        level: 'info',
+        filename: './logs/all-logs.log',
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, //5MB
+        maxFiles: 5,
+        colorize: false
+      }),
+      new winston.transports.Console({
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+      })
+    ],
+    exitOnError: false
+  });
 
-let logger = new winston.Logger({
-  transports: [
-    /*new winston.transports.File({
-      level: 'info',
-      filename: './logs/all-logs.log',
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, //5MB
-      maxFiles: 5,
-      colorize: false
-    }), */
-    new winston.transports.Console({
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    })
-  ],
-  exitOnError: false
-});
+  module.exports = logger;
+  module.exports.stream = {
+    write: function (message) {
+      logger.info(message);
+    }
+  };
+}else {
+  let logger = new winston.Logger({
+    transports: [
+      new winston.transports.Console({
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+      })
+    ],
+    exitOnError: false
+  });
 
-module.exports = logger;
-module.exports.stream = {
-  write: function(message){
-    logger.info(message);
-  }
-};
+  module.exports = logger;
+  module.exports.stream = {
+    write: function (message) {
+      logger.info(message);
+    }
+  };
+}
